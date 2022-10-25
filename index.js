d6eb9b8484fada8ff146b3a5da9f67dde0713325
shourov hasan <shourovhasan172@gmail.com>
2022-10-25 12:50:20 +0600

    install cors and created categories json file

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+
+const categories = require('./data/categories.json');
+
+
+
 app.get('/',(req, res) =>{
   res.send('Course Server Running');
+})
+
+app.get('/course-categories',(req, res) =>{
+  res.send(categories);
 })
 
 
