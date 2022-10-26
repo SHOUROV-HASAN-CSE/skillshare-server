295fa1a20574adef30d823258dd377dc0cb913c6
shourov hasan <shourovhasan172@gmail.com>
2022-10-26 17:15:58 +0600

    hosting in vercel

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
-const categories = require('./data/categories.json');
 
+const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 
 app.get('/',(req, res) =>{
@@ -15,6 +16,17 @@ app.get('/',(req, res) =>{
 
 app.get('/course-categories',(req, res) =>{
   res.send(categories);
+})
+
+app.get('/course',(req, res) =>{
+  res.send(course);
+})
+
+app.get('/category/:id',(req, res) =>{
+  const id = req.params.id;
+  const selectedCourse = course.find( n => n.category_id === id );
+  res.send(selectedCourse);
+  
 })
 
 
